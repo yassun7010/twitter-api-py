b6bf8e4cc5be89a1796fbb83f0a352b7f3a0289d
yassun4dev <yassun4dev@outlook.com>
2023-04-02 00:42:51 +0900

    Update: Twitter API の生成関数のテストケースを増やしました。

--- tests/twitter_api/client/test_twitter_api_client.py
+++ tests/twitter_api/client/test_twitter_api_client.py
@@ -1,6 +1,8 @@
 import pytest
 
+from tests.conftest import synthetic_monitoring_is_disable
 from twitter_api.client.twitter_api_client import TwitterApiClient
+from twitter_api.client.twitter_api_real_client import TwitterApiRealClient
 
 
 class TestTwitterApiClient:
@@ -17,3 +19,24 @@ class TestTwitterApiClient:
         """
         with pytest.raises(TypeError):
             TwitterApiClient()  # type: ignore
+
+    @pytest.mark.skipif(**synthetic_monitoring_is_disable())
+    def test_client_from_bearer_token(self):
+        assert isinstance(
+            TwitterApiClient.from_bearer_token_env(),
+            TwitterApiRealClient,
+        )
+
+    @pytest.mark.skipif(**synthetic_monitoring_is_disable())
+    def test_client_from_app_auth_v2_env(self):
+        assert isinstance(
+            TwitterApiClient.from_app_auth_v2_env(),
+            TwitterApiRealClient,
+        )
+
+    @pytest.mark.skipif(**synthetic_monitoring_is_disable())
+    def test_client_from_user_auth_v1_env(self):
+        assert isinstance(
+            TwitterApiClient.from_user_auth_v1_env(),
+            TwitterApiRealClient,
+        )
--- tests/twitter_api/client/test_twitter_api_real_client.py
+++ tests/twitter_api/client/test_twitter_api_real_client.py
@@ -1,5 +1,32 @@
+import pytest
+
+from tests.conftest import synthetic_monitoring_is_disable
+from twitter_api.client.twitter_api_real_client import TwitterApiRealClient
+
+
 class TestTwitterApiRealClient:
-    def test_real_client(self, real_client):
+    def test_real_client(self, real_client: TwitterApiRealClient):
         # インターフェースの未実装がないかをテストする。
         # TestTwitterApiMockClient はテストで必ずテストされるので、テスト不要。
-        assert True
+        assert isinstance(real_client, TwitterApiRealClient)
+
+    @pytest.mark.skipif(**synthetic_monitoring_is_disable())
+    def test_real_client_from_bearer_token(self):
+        assert isinstance(
+            TwitterApiRealClient.from_bearer_token_env(),
+            TwitterApiRealClient,
+        )
+
+    @pytest.mark.skipif(**synthetic_monitoring_is_disable())
+    def test_real_client_from_app_auth_v2_env(self):
+        assert isinstance(
+            TwitterApiRealClient.from_app_auth_v2_env(),
+            TwitterApiRealClient,
+        )
+
+    @pytest.mark.skipif(**synthetic_monitoring_is_disable())
+    def test_real_client_from_user_auth_v1_env(self):
+        assert isinstance(
+            TwitterApiRealClient.from_user_auth_v1_env(),
+            TwitterApiRealClient,
+        )
