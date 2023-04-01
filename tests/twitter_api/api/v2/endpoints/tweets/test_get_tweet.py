3ec5e7690304d09c5cbac0771373dd34ab7e80cc
yassun4dev <yassun4dev@outlook.com>
2023-04-01 19:01:57 +0900

    Add: テストケースを追加しました。

--- tests/twitter_api/api/v2/endpoints/tweets/test_get_tweet.py
+++ tests/twitter_api/api/v2/endpoints/tweets/test_get_tweet.py
@@ -1,12 +1,41 @@
+from textwrap import dedent
 from twitter_api.api.v2.endpoints.tweets.get_tweet import GetTweetResponseBody
 from twitter_api.api.v2.types.tweet.tweet import Tweet
 from twitter_api.client.twitter_api_mock_client import TwitterApiMockClient
+from twitter_api.client.twitter_api_real_client import (
+    TwitterApiRealClient,
+)
+
+
+class TestTwitterApiClient:
+    def test_get_tweets(self, real_client: TwitterApiRealClient):
+        tweet = Tweet(
+            id="1460323737035677698",
+            text=dedent(
+                # flake8: noqa E501
+                """
+                Introducing a new era for the Twitter Developer Platform! \n
+                📣The Twitter API v2 is now the primary API and full of new features
+                ⏱Immediate access for most use cases, or apply to get more access for free
+                📖Removed certain restrictions in the Policy
+                https://t.co/Hrm15bkBWJ https://t.co/YFfCDErHsg
+                """
+            ).strip(),
+            edit_history_tweet_ids=["1460323737035677698"],
+        )
+        response = GetTweetResponseBody(data=tweet)
+        real_response = real_client.request("/2/tweets/:id").get(tweet.id)
+
+        print(real_response.dict())
+        print(response.dict())
+
+        assert real_response == response
 
 
 class TestTwitterApiMockClient:
     def test_mock_get_tweets(self, mock_client: TwitterApiMockClient):
         tweet = Tweet(
-            id="12345",
+            id="1460323737035677698",
             text="tweet",
             edit_history_tweet_ids=["56789"],
         )
