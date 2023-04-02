cf63ef619dc8d5f8970b4d7a1a4cfd74cdac9ddb
yassun4dev <yassun4dev@outlook.com>
2023-04-02 19:40:30 +0900

    Update: 変数名を変えました。

--- twitter_api/rate_limit/manager/dict_rate_limit_manager.py
+++ twitter_api/rate_limit/manager/dict_rate_limit_manager.py
@@ -10,7 +10,7 @@ from twitter_api.rate_limit.rate_limit_data import RateLimitData
 @dataclass
 class RateLimitStatus:
     start_at: datetime
-    requests: list[datetime]
+    request_datetimes: list[datetime]
 
 
 class DictRateLimitManager(RateLimitManager):
@@ -26,16 +26,18 @@ class DictRateLimitManager(RateLimitManager):
             now = datetime.now()
 
         if rate_limit not in self._store:
-            self._store[rate_limit] = RateLimitStatus(start_at=now, requests=[now])
+            self._store[rate_limit] = RateLimitStatus(
+                start_at=now, request_datetimes=[now]
+            )
 
         # レートリミットの窓に今回のデータを入れる。
         status = self._store[rate_limit]
-        status.requests.append(now)
+        status.request_datetimes.append(now)
 
         # 窓に入っている過去のデータのうち、考慮する必要のないデータを消す。
         min_datetime = now - timedelta(seconds=rate_limit.total_seconds)
-        index = bisect_left(status.requests, min_datetime)
-        del status.requests[:index]
+        index = bisect_left(status.request_datetimes, min_datetime)
+        del status.request_datetimes[:index]
 
         # 窓に入っているデータの数が、制限を超えていたらリミットオーバ
-        return len(status.requests) > rate_limit.requests
+        return len(status.request_datetimes) > rate_limit.requests
