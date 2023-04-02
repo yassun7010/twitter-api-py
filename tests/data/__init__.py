408b4d7efb8509a6df8ccf9574a9856a6a491b7f
yassun4dev <yassun4dev@outlook.com>
2023-04-02 22:45:40 +0900

    Update: JSON ファイルのパスを取得する関数を追加。

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -4,5 +4,9 @@ from pathlib import Path
 
 class JsonDataLoader:
     def load(self, filename: str) -> dict:
-        with open(Path(__file__).parent / filename) as file:
+        with open(get_json_data_filepath(filename)) as file:
             return json.load(file)
+
+
+def get_json_data_filepath(filename: str) -> Path:
+    return Path(__file__).parent / filename
