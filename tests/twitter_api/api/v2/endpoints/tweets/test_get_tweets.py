e6b68e7d3e584c0d130a7af9f9841921a1e22715
yassun4dev <yassun4dev@outlook.com>
2023-04-03 01:00:42 +0900

    Update: テストケースを修正しました。

--- tests/twitter_api/api/v2/endpoints/tweets/test_get_tweets.py
+++ tests/twitter_api/api/v2/endpoints/tweets/test_get_tweets.py
@@ -61,6 +61,8 @@ class TestMockV2GetTweets:
             mock_app_auth_v2_client.chain()
             .inject_get_response_body("https://api.twitter.com/2/tweets", response)
             .request("https://api.twitter.com/2/tweets")
-            .get({"ids": list(map(lambda tweet: tweet.id, response.data))})
+            .get(
+                {"ids": "1460323737035677698", "expansions": ["attachments.media_keys"]}
+            )
             == response
         )
