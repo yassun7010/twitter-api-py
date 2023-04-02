d9c71e4d22a5d5cd7dbb6dd2530da42d92638fa6
yassun4dev <yassun4dev@outlook.com>
2023-04-02 22:09:08 +0900

    Update: コメントを修正しました。

--- twitter_api/client/twitter_api_client.py
+++ twitter_api/client/twitter_api_client.py
@@ -31,6 +31,9 @@ class TwitterApiClient(metaclass=ABCMeta):
     def chain(self) -> Self:
         """
         メソッドチェーンをキレイに表示させるための関数。
+
+        あくまで black というフォーマッタを用いてコードを書くとき、
+        キレイな見た目にするために使う。ロジック的な意味はない。
         """
 
         return self
