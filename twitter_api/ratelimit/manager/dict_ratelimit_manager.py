32da789a78c80a7c66d480faa05f870fb6000f84
yassun4dev <yassun4dev@outlook.com>
2023-04-02 18:21:33 +0900

    Update: 引数を変更しました。

--- twitter_api/ratelimit/manager/dict_ratelimit_manager.py
+++ twitter_api/ratelimit/manager/dict_ratelimit_manager.py
@@ -1,6 +1,7 @@
 from bisect import bisect_left
 from dataclasses import dataclass
 from datetime import datetime, timedelta
+from typing import Optional
 
 from twitter_api.ratelimit.manager.ratelimit_manager import RatelimitManager
 from twitter_api.ratelimit.ratelimit import Ratelimit
@@ -16,8 +17,14 @@ class DictRatelimitManager(RatelimitManager):
     def __init__(self) -> None:
         self._store: dict[Ratelimit, RatelimitStatus] = {}
 
-    def check_limit_over(self, ratelimit: Ratelimit) -> bool:
-        now = datetime.now()
+    def check_limit_over(
+        self,
+        ratelimit: Ratelimit,
+        now: Optional[datetime] = None,
+    ) -> bool:
+        if now is None:
+            now = datetime.now()
+
         if ratelimit not in self._store:
             self._store[ratelimit] = RatelimitStatus(start_at=now, requests=[now])
 
--- twitter_api/ratelimit/manager/ratelimit_manager.py
+++ twitter_api/ratelimit/manager/ratelimit_manager.py
@@ -1,9 +1,15 @@
 from abc import ABCMeta, abstractmethod
+from datetime import datetime
+from typing import Optional
 
 from twitter_api.ratelimit.ratelimit import Ratelimit
 
 
 class RatelimitManager(metaclass=ABCMeta):
     @abstractmethod
-    def check_limit_over(self, ratelimit: Ratelimit) -> bool:
+    def check_limit_over(
+        self,
+        ratelimit: Ratelimit,
+        now: Optional[datetime] = None,
+    ) -> bool:
         ...
