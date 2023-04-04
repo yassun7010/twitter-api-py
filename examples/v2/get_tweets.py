7dfcbd443ffdf103d70e04ad555bd2eb49364a56
yassun4dev <yassun4dev@outlook.com>
2023-04-05 00:24:32 +0900

    Update: サンプルを修正。

--- examples/v2/get_tweets.py
+++ examples/v2/get_tweets.py
@@ -6,9 +6,14 @@ from twitter_api.error import TwitterApiError
 try:
     client = TwitterApiClient.from_app_auth_v2_env()
 
-    tweets = client.request("https://api.twitter.com/2/tweets").get(
-        {"ids": ["1460323737035677698"]},
+    tweets = (
+        client.chain()
+        .request("https://api.twitter.com/2/tweets")
+        .get(
+            {"ids": ["1460323737035677698"]},
+        )
     )
+
     print(tweets)
 
 except TwitterApiError as error:
