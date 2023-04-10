bd737d71d6d874ecf1f83e22245cd760a99010a3
yassun4dev <yassun4dev@outlook.com>
2023-04-10 23:27:32 +0900

    Fix: コメントを修正。

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -3,9 +3,7 @@ from pathlib import Path
 
 def json_test_data(filename: str) -> Path:
     """
-    自動テスト用データのファイルパスを返す。
-
-    自動テスト用のデータを作成したいときに使う。
+    自動テスト用データの絶対ファイルパスを返す。
     """
 
     return Path(__file__).parent / filename
