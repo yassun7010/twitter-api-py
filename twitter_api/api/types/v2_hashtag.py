90470f26a55137848f83d3dd0386155b332994ef
yassun4dev <yassun4dev@outlook.com>
2023-04-24 01:31:42 +0900

    Update: SearchQuery を検討するため、いったん hashtag の型は消しておく。

--- twitter_api/api/types/v2_hashtag.py
+++
@@ -1,3 +0,0 @@
-from typing import TypeAlias
-
-Hashtag: TypeAlias = str
--- twitter_api/api/types/v2_retweet/retweet_entities_description_hashtag.py
+++ twitter_api/api/types/v2_retweet/retweet_entities_description_hashtag.py
@@ -1,8 +1,7 @@
-from twitter_api.api.types.v2_hashtag import Hashtag
 from twitter_api.types.extra_permissive_model import ExtraPermissiveModel
 
 
 class RetweetEntitiesDescriptionHashtag(ExtraPermissiveModel):
     start: int
     end: int
-    hashtag: Hashtag
+    hashtag: str
