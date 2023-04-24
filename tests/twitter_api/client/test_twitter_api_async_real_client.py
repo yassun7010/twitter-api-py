da1c8eb98a5fe03b245ba2ec41bb36159fdc4de9
yassun4dev <yassun4dev@outlook.com>
2023-04-24 10:26:24 +0900

    Remove: 不要になった自動テストを削除。

--- tests/twitter_api/client/test_twitter_api_async_real_client.py
+++ tests/twitter_api/client/test_twitter_api_async_real_client.py
@@ -6,12 +6,6 @@ from twitter_api.client.twitter_api_async_real_client import TwitterApiAsyncReal
 
 class TestTwitterApiAsyncRealClient:
     @pytest.mark.skipif(**synthetic_monitoring_is_disable())
-    def test_real_client(self, oauth2_app_async_real_client: TwitterApiAsyncRealClient):
-        # インターフェースの未実装がないかをテストする。
-        # TestTwitterApiMockClient はテストで必ずテストされるので、テスト不要。
-        assert isinstance(oauth2_app_async_real_client, TwitterApiAsyncRealClient)
-
-    @pytest.mark.skipif(**synthetic_monitoring_is_disable())
     def test_real_client_from_oauth2_bearer_token(self):
         assert isinstance(
             TwitterApiAsyncRealClient.from_oauth2_bearer_token_env(),
--- tests/twitter_api/client/test_twitter_api_real_client.py
+++ tests/twitter_api/client/test_twitter_api_real_client.py
@@ -6,12 +6,6 @@ from twitter_api.client.twitter_api_real_client import TwitterApiRealClient
 
 class TestTwitterApiRealClient:
     @pytest.mark.skipif(**synthetic_monitoring_is_disable())
-    def test_real_client(self, oauth2_app_real_client: TwitterApiRealClient):
-        # インターフェースの未実装がないかをテストする。
-        # TestTwitterApiMockClient はテストで必ずテストされるので、テスト不要。
-        assert isinstance(oauth2_app_real_client, TwitterApiRealClient)
-
-    @pytest.mark.skipif(**synthetic_monitoring_is_disable())
     def test_real_client_from_oauth2_bearer_token(self):
         assert isinstance(
             TwitterApiRealClient.from_oauth2_bearer_token_env(),
