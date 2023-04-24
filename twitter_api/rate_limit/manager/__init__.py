f722c4fcff866e6b3e8734a1c1ba8252f4971502
Yasutanium <yassun4dev@outlook.com>
2023-04-25 02:24:57 +0900

    Add: コメントを追加。

--- twitter_api/rate_limit/manager/__init__.py
+++ twitter_api/rate_limit/manager/__init__.py
@@ -3,3 +3,9 @@ from twitter_api.rate_limit.manager.no_operation_rate_limit_manager import (
 )
 
 DEFAULT_RATE_LIMIT_MANAGER = NoOperationRateLimitManager()
+"""
+デフォルトのレートリミットマネージャ。
+
+NoOperationRateLimitManager は状態を持たない実装のため、
+関数の引数のデフォルト値として与えても問題のないマネージャである。
+"""
