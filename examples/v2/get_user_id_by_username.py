af199ac58facb90e587c6ece5035a243f815ea8e
yassun4dev <yassun4dev@outlook.com>
2023-04-26 23:28:25 +0900

    Update: サンプルを追加。

--- examples/v2/get_user_id_by_username.py
+++ examples/v2/get_user_id_by_username.py
@@ -4,24 +4,24 @@ from twitter_api.client.twitter_api_client import TwitterApiClient
 from twitter_api.error import TwitterApiError
 
 if len(sys.argv) == 1:
-    sys.stderr.write("Please input user id: ")
-    user_id = input()
+    sys.stderr.write("Please input username: ")
+    username = input()
 elif len(sys.argv) == 2:
-    user_id = sys.argv[1]
+    username = sys.argv[1]
 else:
-    print("Usage: python get_user_name_by_user_id.py [USER_ID]", file=sys.stderr)
+    print("Usage: python get_user_id_by_username.py [USER_ID]", file=sys.stderr)
     sys.exit(1)
 
 try:
     with TwitterApiClient.from_oauth2_app_env() as client:
         user = (
             client.chain()
-            .request("https://api.twitter.com/2/users/:id")
-            .get(user_id)
+            .request("https://api.twitter.com/2/users/by/username/:username")
+            .get(username)
             .data
         )
 
-        print(user.username)
+        print(user.id)
 
 except TwitterApiError as error:
     print(error.info.json(indent=2), file=sys.stderr)
