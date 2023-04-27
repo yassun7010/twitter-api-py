3121a821c667bcab58f061caae9fbe54edd004eb
yassun4dev <yassun4dev@outlook.com>
2023-04-27 09:51:32 +0900

    Add: テストケースを追加しました。

--- tests/twitter_api/api/resources/v2_users_by/test_get_v2_by_users.py
+++ tests/twitter_api/api/resources/v2_users_by/test_get_v2_by_users.py
@@ -6,9 +6,13 @@ from tests.data import json_test_data
 from twitter_api.api.resources.v2_users_by.get_v2_users_by import (
     GetV2UsersByResponseBody,
 )
+from twitter_api.api.types.v2_tweet.tweet_field import ALL_PUBLIC_TWEET_FIELDS
 from twitter_api.api.types.v2_user.user import User
+from twitter_api.api.types.v2_user.user_expantion import ALL_USER_EXPANSIONS
+from twitter_api.api.types.v2_user.user_field import ALL_USER_FIELDS
 from twitter_api.client.twitter_api_async_mock_client import TwitterApiAsyncMockClient
 from twitter_api.client.twitter_api_mock_client import TwitterApiMockClient
+from twitter_api.client.twitter_api_real_client import TwitterApiRealClient
 from twitter_api.types.extra_permissive_model import get_extra_fields
 
 
@@ -40,6 +44,28 @@ class TestGetV2UsersBy:
             print(response_body.json())
 
             assert get_extra_fields(response_body) == {}
+
+    def test_get_v2_users_by_all_fields(
+        self,
+        oauth2_app_real_client: TwitterApiRealClient,
+        twitter_dev_user: User,
+    ):
+        response_body = (
+            oauth2_app_real_client.chain()
+            .request("https://api.twitter.com/2/users/by")
+            .get(
+                {
+                    "usernames": [twitter_dev_user.username],
+                    "expansions": ALL_USER_EXPANSIONS,
+                    "tweet.fields": ALL_PUBLIC_TWEET_FIELDS,
+                    "user.fields": ALL_USER_FIELDS,
+                }
+            )
+        )
+
+        print(response_body.json())
+
+        assert get_extra_fields(response_body) == {}
 
 
 class TestMockGetV2UsersBy:
