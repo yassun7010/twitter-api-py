6fa8d2bdbe658f1373202adb03e668153fd9f429
yassun4dev <yassun4dev@outlook.com>
2023-05-01 13:03:02 +0900

    Add: 自動テストを追加。

--- twitter_api/utils/json.py
+++ twitter_api/utils/json.py
@@ -1,3 +1,11 @@
+"""
+JSON のデータをそのまま Python のコードとして使えるようにするための定数定義。
+
+>>> from twitter_api.utils.json import *
+>>> {"a": null, "b": 1, "c": "2", "d": true, "e": false, "f": [3, 4], "g": {"h": 5}}
+{'a': None, 'b': 1, 'c': '2', 'd': True, 'e': False, 'f': [3, 4], 'g': {'h': 5}}
+"""
+
 null = None
 """Json のデータをそのままコードにコピーして利用できるように、 null を定義しておく。"""
 
