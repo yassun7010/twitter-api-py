ffc80f0647dca0505a830a1900c4635a9330d93d
yassun4dev <yassun4dev@outlook.com>
2023-05-01 14:35:14 +0900

    Add: バージョン情報を追加。

--- twitter_api/__init__.py
+++ twitter_api/__init__.py
@@ -1,7 +1,11 @@
+import importlib.metadata
+
 from .client.twitter_api_async_client import TwitterApiAsyncClient
 from .client.twitter_api_async_mock_client import TwitterApiAsyncMockClient
 from .client.twitter_api_client import TwitterApiClient
 from .client.twitter_api_mock_client import TwitterApiMockClient
+
+__version__ = importlib.metadata.version("twitter_api_py")
 
 __all__ = [
     "TwitterApiAsyncClient",
