3c776eba48ffc6f9cead463ef521a8602048ad98
yassun4dev <yassun4dev@outlook.com>
2023-05-03 01:32:51 +0900

    Update: 型名を修正。

--- twitter_api/types/oauth1/oauth1_access_token.py
+++ twitter_api/types/oauth1/oauth1_access_token.py
@@ -5,13 +5,14 @@ from twitter_api.types.extra_permissive_model import ExtraPermissiveModel
 from twitter_api.types.generic_client import TwitterApiGenericClient
 from twitter_api.types.oauth import AccessSecret, AccessToken
 from twitter_api.types.v2_user.user_id import UserId
+from twitter_api.types.v2_user.username import Username
 
 
 class OAuth1AccessToken(Generic[TwitterApiGenericClient], ExtraPermissiveModel):
     oauth_token: AccessToken
     oauth_token_secret: AccessSecret
     user_id: UserId
-    screen_name: str
+    screen_name: Username
     _session: TwitterOAuth1Session[TwitterApiGenericClient]
 
     def generate_client(self) -> TwitterApiGenericClient:
