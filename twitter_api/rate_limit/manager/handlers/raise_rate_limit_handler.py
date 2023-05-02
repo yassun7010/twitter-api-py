e47933674481542d056061c47f128cdfd3303081
Yasutanium <yassun4dev@outlook.com>
2023-05-02 23:09:58 +0900

    Remove: 重複したドキュメントを削除。

--- twitter_api/rate_limit/manager/handlers/raise_rate_limit_handler.py
+++ twitter_api/rate_limit/manager/handlers/raise_rate_limit_handler.py
@@ -15,10 +15,6 @@ class RaiseRateLimitHandler(RateLimitManager):
     def handle_rate_limit_sync(
         self, rate_limit_info: RateLimitInfo
     ) -> Generator[None, None, None]:
-        """
-        同期的な TwitterApiClient を用いている場合のレートリミットの処理方法。
-        """
-
         if self.check_limit_over(rate_limit_info) is not None:
             raise RateLimitOverError(rate_limit_info)
 
@@ -28,10 +24,6 @@ class RaiseRateLimitHandler(RateLimitManager):
     async def handle_rate_limit_async(
         self, rate_limit_info: RateLimitInfo
     ) -> AsyncGenerator[None, None]:
-        """
-        非同期的な TwitterApiAsyncClient を用いている場合のレートリミットの処理方法。
-        """
-
         if self.check_limit_over(rate_limit_info) is not None:
             raise RateLimitOverError(rate_limit_info)
 
