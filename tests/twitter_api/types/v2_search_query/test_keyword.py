93e97d023099b65d258689c39aa9b86082ce4ae4
yassun4dev <yassun4dev@outlook.com>
2023-05-04 02:52:06 +0900

    Update: テストケースを修正。

--- tests/twitter_api/types/v2_search_query/test_keyword.py
+++ tests/twitter_api/types/v2_search_query/test_keyword.py
@@ -6,5 +6,11 @@ class TestKeyword:
     def test_keyword(self):
         assert str(Keyword("test")) == "test"
 
+    def test_keyword_with_phrase(self):
+        assert str(Keyword("test twitter")) == '"test twitter"'
+
+    def test_keyword_with_phrase_double_quotes(self):
+        assert str(Keyword('"test" twitter')) == r'"\"test\" twitter"'
+
     def test_query_build(self):
         assert str(SearchQuery.build(lambda q: q.keyword("test"))) == "test"
