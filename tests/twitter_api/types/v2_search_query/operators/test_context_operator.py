fe69949c5af24f0c09e98f192477ecbc5f748584
yassun4dev <yassun4dev@outlook.com>
2023-05-04 12:30:06 +0900

    Fix: 自動テストを修正しました。

--- tests/twitter_api/types/v2_search_query/operators/test_context_operator.py
+++ tests/twitter_api/types/v2_search_query/operators/test_context_operator.py
@@ -1,22 +1,46 @@
+import pytest
+
+from twitter_api.types.v2_domain import Domain
+from twitter_api.types.v2_entity.entity import Entity
 from twitter_api.types.v2_search_query.operators.context_operator import ContextOperator
 from twitter_api.types.v2_search_query.search_query import SearchQuery
+from twitter_api.types.v2_tweet.tweet_context_annotation import TweetContextAnnotation
+
+
+@pytest.fixture
+def context():
+    return TweetContextAnnotation(
+        domain=Domain(
+            id="119",
+            name="Holiday",
+            description="Holidays like Christmas or Halloween",
+        ),
+        entity=Entity(id="1186637514896920576", name=" New Years Eve"),
+    )
 
 
 class TestContext:
-    def test_context(self):
-        assert (
-            str(ContextOperator("10.799022225751871488"))
-            == "context:10.799022225751871488"
-        )
+    def test_context(self, context):
+        assert str(ContextOperator(context)) == "context:119.1186637514896920576"
 
-    def test_context_by_ids(self):
+    def test_context_from_ids(self):
         assert (
             str(ContextOperator(domain_id="10", entity_id="799022225751871488"))
             == "context:10.799022225751871488"
         )
 
-    def test_query_build(self):
+    def test_query_build(self, context):
         assert (
-            str(SearchQuery.build(lambda q: q.context("1539382664746020864")))
-            == "context:1539382664746020864"
+            str(SearchQuery.build(lambda q: q.context(context)))
+            == "context:119.1186637514896920576"
+        )
+
+    def test_query_build_from_ids(self):
+        assert (
+            str(
+                SearchQuery.build(
+                    lambda q: q.context(domain_id="10", entity_id="799022225751871488")
+                )
+            )
+            == "context:10.799022225751871488"
         )
