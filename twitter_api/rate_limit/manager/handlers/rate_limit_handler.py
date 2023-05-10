0d96b114c6a4edf9ec88f8e4defeaffd6b5c7f5a
yassun4dev <yassun4dev@outlook.com>
2023-05-10 22:42:32 +0900

    Fix: 継承関係を調整。

--- twitter_api/rate_limit/manager/handlers/rate_limit_handler.py
+++ twitter_api/rate_limit/manager/handlers/rate_limit_handler.py
@@ -1,29 +1,7 @@
-from abc import ABCMeta, abstractmethod
-from contextlib import asynccontextmanager, contextmanager
-from typing import AsyncGenerator, Generator
-
-from twitter_api.rate_limit.rate_limit_info import RateLimitInfo
+from twitter_api.rate_limit.manager.rate_limit_manager import RateLimitManager
 
 
-class RateLimitHandler(metaclass=ABCMeta):
-    @abstractmethod
-    @contextmanager
-    def handle_rate_limit_sync(
-        self, rate_limit_info: RateLimitInfo
-    ) -> Generator[None, None, None]:
-        """
-        同期的な TwitterApiClient を用いている場合のレートリミットの対応方法。
-        """
+class RateLimitHandler(RateLimitManager):
+    """レートリミットの操作部分を部品化するため、クラスを切り出している。"""
 
-        ...
-
-    @abstractmethod
-    @asynccontextmanager
-    async def handle_rate_limit_async(
-        self, rate_limit_info: RateLimitInfo
-    ) -> AsyncGenerator[None, None]:
-        """
-        非同期的な TwitterApiAsyncClient を用いている場合のレートリミットの対応方法。
-        """
-
-        ...
+    pass
--- twitter_api/rate_limit/manager/rate_limit_manager.py
+++ twitter_api/rate_limit/manager/rate_limit_manager.py
@@ -1,5 +1,42 @@
-from twitter_api.rate_limit.manager.handlers.rate_limit_handler import RateLimitHandler
+from abc import ABCMeta, abstractmethod
+from contextlib import asynccontextmanager, contextmanager
+from datetime import datetime
+from typing import AsyncGenerator, Generator, Optional
+
+from twitter_api.rate_limit.rate_limit_info import RateLimitInfo
 
 
-class RateLimitManager(RateLimitHandler):
-    pass
+class RateLimitManager(metaclass=ABCMeta):
+    @abstractmethod
+    def check_limit_over(
+        self,
+        rate_limit_info: RateLimitInfo,
+        now: Optional[datetime] = None,
+    ) -> Optional[float]:
+        """
+        レートリミットオーバーかを調べ、超えている場合は必要な待ち時間[秒]を返す。
+        """
+
+        ...
+
+    @abstractmethod
+    @contextmanager
+    def handle_rate_limit_sync(
+        self, rate_limit_info: RateLimitInfo
+    ) -> Generator[None, None, None]:
+        """
+        同期的な TwitterApiClient を用いている場合のレートリミットの対応方法。
+        """
+
+        ...
+
+    @abstractmethod
+    @asynccontextmanager
+    async def handle_rate_limit_async(
+        self, rate_limit_info: RateLimitInfo
+    ) -> AsyncGenerator[None, None]:
+        """
+        非同期的な TwitterApiAsyncClient を用いている場合のレートリミットの対応方法。
+        """
+
+        ...
