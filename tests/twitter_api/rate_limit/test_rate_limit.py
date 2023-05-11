d6d03882703eae967e42f00ef529d4dc262f587d
yassun4dev <yassun4dev@outlook.com>
2023-05-11 22:14:20 +0900

    Add: テストケースの追加。

--- tests/twitter_api/rate_limit/test_rate_limit.py
+++ tests/twitter_api/rate_limit/test_rate_limit.py
@@ -1,9 +1,14 @@
+from contextlib import asynccontextmanager, contextmanager
+from datetime import datetime
+from typing import AsyncGenerator, Generator, Optional
+
 import pytest
 
 from twitter_api.client.request.request_mock_client import RequestMockClient
 from twitter_api.rate_limit.manager import DEFAULT_RATE_LIMIT_MANAGER
 from twitter_api.rate_limit.manager.rate_limit_manager import RateLimitManager
 from twitter_api.rate_limit.rate_limit import rate_limit
+from twitter_api.rate_limit.rate_limit_info import RateLimitInfo
 from twitter_api.resources.api_resources import ApiResources
 from twitter_api.types.endpoint import Endpoint
 
@@ -35,3 +40,34 @@ class TestRateLimit:
 
         with pytest.raises(ValueError):
             handle(api_resource(DEFAULT_RATE_LIMIT_MANAGER))
+
+    def test_rate_limit_when_error_handle(self):
+        @rate_limit(TEST_ENDPOINT, "app", requests=500, mins=15)
+        def handle(self: ApiResources):
+            raise ValueError()
+
+        class IgnoreValueErrorRateLimitManager(RateLimitManager):
+            def check_limit_over(
+                self, rate_limit_info: RateLimitInfo, now: Optional[datetime] = None
+            ) -> Optional[float]:
+                return None
+
+            @contextmanager
+            def handle_rate_limit_sync(
+                self, rate_limit_info: RateLimitInfo
+            ) -> Generator[None, None, None]:
+                try:
+                    yield
+                except ValueError:
+                    pass
+
+            @asynccontextmanager
+            async def handle_rate_limit_async(
+                self, rate_limit_info: RateLimitInfo
+            ) -> AsyncGenerator[None, None]:
+                try:
+                    yield
+                except ValueError:
+                    pass
+
+        assert handle(api_resource(IgnoreValueErrorRateLimitManager())) is None
